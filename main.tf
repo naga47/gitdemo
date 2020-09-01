4640fa0fb3e1a8b9a9c97a5ea878dbe2e801cb21
naga <nagarjunanetha@gmail.com>
2020-09-01 11:02:39 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-resource "aws_vpc" "myvpc" {
-    cidr = 10.0.0.0/16
-    absc = "naga"
-    
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
